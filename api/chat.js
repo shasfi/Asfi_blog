2587366b6477c30cbecd6fa3ce204e1c4c05c264
shasfi <faasfi7222@gmail.com>
2026-07-10 00:16:43 +0500

    change

--- api/chat.js
+++ api/chat.js
@@ -71,7 +71,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "google/gemini-2.0-flash-exp:free",
+        model: "openrouter/free",
         messages: [systemPrompt, ...messages],
         max_tokens: 400,
       }),
